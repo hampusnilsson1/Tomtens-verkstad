258f8a045de208ed155e6ea57b1ca73a16d0cf41
Lotta Larsson <147097558+Gammaltant@users.noreply.github.com>
2023-11-22 19:32:07 +0100

    Uppdatering av önskelistan i form

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
     JFrame logInFrame;
@@ -11,6 +13,9 @@ public class Login {
     private JButton logInButton;
     private JButton registreraButton;
 
+    private String användarnamn = "Julen";
+    private String lösenord = "2023";
+
     public Login()
     {
         logInFrame = new JFrame();
@@ -19,5 +24,28 @@ public class Login {
         logInFrame.setContentPane(loginPanel);
         logInFrame.setResizable(false);
         logInFrame.setVisible(true);
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String användarnamn = användarField.getText();
+                String lösenord = lösenordField.getText();
+
+                boolean kontrolleraAnvändare;
+                if(användarField.getText().equals(användarnamn)
+                    && lösenordField.getText().equals(lösenord)){
+                    logInFrame.setVisible(false);
+                    TomtensView tomtensView = new TomtensView();
+                    tomtensView.setVisible(true);
+                }else {
+                    System.out.println("Fel inloggningsuppgifter");
+                }
+            }
+        });
+        registreraButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
--- src/main/java/org/example/TomtensView.java
+++ src/main/java/org/example/TomtensView.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class TomtensView {
+public class TomtensView extends JFrame{
     JFrame tomtensViewFrame;
     private JPanel tomtensVeiw;
     private JButton lagersaldoButton;
