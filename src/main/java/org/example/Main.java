6f35f281635d2cdd1a562a36f7ef30b8bdc55feb
Agronomety <109460572+Agronomety@users.noreply.github.com>
2023-11-23 16:31:37 +0100

    Added wishlist + wishlistform. Added functions to add and remove from the wishlist.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("VÅR GRUPPUPPGIFT TOMTENSVERKSTAD");
         System.out.println("test");
         Login login = new Login();
+        Wishlist wishlist = new Wishlist();
+
     }
 }
