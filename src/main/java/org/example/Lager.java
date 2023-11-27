64ca3aa3cb58ebca25dab321ca5f1252978fe5eb
osten2600 <kjellberg.olle@outlook.com>
2023-11-27 13:30:16 +0100

    aw

---
+++ src/main/java/org/example/Lager.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Lager extends JFrame{
+    JFrame LagerVeiwFrame;
+    private JPanel LagerVeiw;
+    private JList WarehouseList;
+
+    public Lager() {
+        Fi
+    }
+}
