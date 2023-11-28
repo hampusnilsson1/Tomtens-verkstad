274d14fdc2470d71ac61cc1b1d5b8bea597c0fb2
osten2600 <kjellberg.olle@outlook.com>
2023-11-28 14:16:03 +0100

    wa

--- src/main/java/org/example/Lager.java
+++ src/main/java/org/example/Lager.java
@@ -7,8 +7,14 @@ public class Lager extends JFrame{
     JFrame LagerVeiwFrame;
     private JPanel LagerVeiw;
     private JList WarehouseList;
+    private JButton button1;
+    private JPanel WarehousPannel;
 
     public Lager() {
-        Fi
+        LagerVeiwFrame = new JFrame();
+        LagerVeiwFrame.setSize(500, 500);
+        LagerVeiwFrame.setVisible(true);
+        LagerVeiwFrame.setContentPane(WarehousPannel);
+        LagerVeiwFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 }
