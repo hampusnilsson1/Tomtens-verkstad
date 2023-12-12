10e7faa6e2ef83e779c67c141b52262255204702
osten2600 <kjellberg.olle@outlook.com>
2023-12-12 15:04:38 +0100

    Create Wish.java

---
+++ src/main/java/org/example/Wish.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Wish {
+
+    private int wishId;
+    private String wishName;
+    private int quantity;
+
+    public Wish(String wishName){
+        this.wishName=wishName;
+    }
+    public Wish(int wishId, String wishName, int quantity){
+        this.wishId = wishId;
+        this.wishName = wishName;
+        this.quantity = quantity;
+    }
+
+    public void printWishes(){
+        System.out.println(wishId + ". "+ wishName );
+    }
+    public int getWishId() {
+        return wishId;
+    }
+
+    public void setWishId(int wishId) {
+        this.wishId = wishId;
+    }
+
+    public String getWishName() {
+        return wishName;
+    }
+
+    public void setWishName(String wishName) {
+        this.wishName = wishName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
